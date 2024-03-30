63acb1fb3d36df6d54ef24b0f7363c88d7de01ed
Robert <123roabel@gmail.com>
2024-03-29 20:44:30 -0400

    fix review

--- models/review.py
+++ models/review.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-""" Review Module for HBNB project """
-from models.base_model import BaseModel
+""" Review module for the HBNB project """
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
 
 
-class Review(BaseModel):
-    """ Review class """
+class Review(BaseModel, Base):
+    """ Review class to store review information """
     __tablename__ = 'reviews'
 
     text = Column(String(1024), nullable=False)
