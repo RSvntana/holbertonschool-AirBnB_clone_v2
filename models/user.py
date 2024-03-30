df1f3fe570c923984e8f1e82e11f7d17500c57c1
Robert <123roabel@gmail.com>
2024-03-29 20:44:15 -0400

    fix user

--- models/user.py
+++ models/user.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
-""" User Module for HBNB project """
-from models.base_model import BaseModel
-from models.review import Review
+"""User Module for HBNB project."""
+
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
-class User(BaseModel):
-    """ User class """
+class User(BaseModel, Base):
+    """User class."""
     __tablename__ = 'users'
 
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-    reviews = relationship('Review', backref='user', cascade='all, delete')
+    places = relationship("Place", backref="user",
+                          cascade="all, delete, delete-orphan")
+    reviews = relationship("Review", backref="user",
+                           cascade="all, delete, delete-orphan")
+
+    def __init__(self, *args, **kwargs):
+        """Initialize User instance."""
+        super().__init__(*args, **kwargs)
