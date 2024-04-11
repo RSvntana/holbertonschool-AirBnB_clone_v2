e9a77885276e282812da62ef1d04b0e98747ce19
Robert <123roabel@gmail.com>
2024-04-11 17:29:54 -0400

    add task 0-6

--- models/state.py
+++ models/state.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy.orm import Session
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
 from os import getenv
 import models
-from models.city import City
 
 
 class State(BaseModel, Base):
@@ -16,16 +14,6 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="state",
                           cascade="all, delete")
-    # @property
-    # def cities(self):
-    #     """Getter attribute cities that
-    # returns the list of City instances"""
-    #     city_list = []
-    #     all_cities = models.storage.all(City)
-    #     for city in all_cities.values():
-    #         if city.state_id == self.id:
-    #             city_list.append(city)
-    #     return city_list
 
     if getenv('HBNB_TYPE_STORAGE') != 'db':
         @property
@@ -37,6 +25,6 @@ class State(BaseModel, Base):
             return [city for city in all_cities.values()
                     if city.state_id == self.id]
 
-        def close(self):
-            """Close session"""
-            Session.close()
+    def close(self):
+        """Close session"""
+        models.storage.close()
